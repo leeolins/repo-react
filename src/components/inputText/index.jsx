e8b225e5c5e54b1123427502d910de0e0a695cc1
Leonardo Lins <leeolins@live.com>
2021-07-10 13:27:54 -0300

    add placeholder no input de pesquisa

--- src/components/inputText/index.jsx
+++ src/components/inputText/index.jsx
@@ -5,6 +5,7 @@ import './styles.css'
 export const InputText = ({searchValue, handleChange}) =>{
     return (
         <input 
+            placeholder='Digite o nome do card'
             className='inputStyle'
             type="search"
             value ={searchValue}
